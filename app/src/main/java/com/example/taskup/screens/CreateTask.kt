5ed56699f8b0f59260e7784512f00b61291d3eff
JoseLeandroNascimento <j.leandro1998silva@gmail.com>
2025-04-16 23:20:06 -0500

    feat: create screen the new task

---
+++ app/src/main/java/com/example/taskup/screens/CreateTask.kt
@@ -0,0 +1,80 @@
+package com.example.taskup.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.taskup.R
+import com.example.taskup.ui.theme.TaskUpTheme
+
+@Composable
+fun CreateTaskScreen(modifier: Modifier = Modifier) {
+
+    var nameTask by remember { mutableStateOf("") }
+    var descriptionTask by remember { mutableStateOf("") }
+
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+        ,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = nameTask,
+            onValueChange = {nameTask = it},
+            label = {
+                Text(text = stringResource(id = R.string.create_task_label_name))
+            }
+        )
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = descriptionTask,
+            maxLines = 3,
+            minLines = 3,
+            onValueChange = {descriptionTask = it},
+            label = {
+                Text(text = stringResource(id = R.string.create_task_label_description))
+            }
+        )
+
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = {},
+            shape = RoundedCornerShape(4.dp)
+            )
+        {
+            Text(text = stringResource(id = R.string.create_task_btn_text_save))
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun CreateTaskScreenPreview() {
+    TaskUpTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            CreateTaskScreen(Modifier.padding(innerPadding).padding(horizontal = 12.dp))
+        }
+    }
+}
