7e21f81b6280c7acf29ec5d49513e3d7924492cd
JoseLeandroNascimento <j.leandro1998silva@gmail.com>
2025-04-18 01:58:27 -0500

    feat: add input date

--- app/src/main/java/com/example/taskup/MainActivity.kt
+++ app/src/main/java/com/example/taskup/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -12,13 +13,16 @@ import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
@@ -55,6 +59,9 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier.fillMaxSize(),
                         topBar = {
                             TopAppBar(
+                                colors = TopAppBarDefaults.topAppBarColors(
+                                    containerColor = if(isSystemInDarkTheme()) MaterialTheme.colorScheme.surface  else  Color.White
+                                ),
                                 title = { Text(text = stringResource(id = R.string.app_name)) },
                                 navigationIcon = {
                                     IconButton(
@@ -77,7 +84,7 @@ class MainActivity : ComponentActivity() {
                         NavHostApp(
                             modifier = Modifier
                                 .padding(innerPadding)
-                                .padding(horizontal = 8.dp),
+                                .padding(horizontal = 8.dp, vertical = 8.dp),
                             navController = navController
                         )
 
--- app/src/main/java/com/example/taskup/screens/CreateTask.kt
+++ app/src/main/java/com/example/taskup/screens/CreateTask.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,10 +14,21 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDefaults
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -24,24 +36,38 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.taskup.R
 import com.example.taskup.ui.theme.TaskUpTheme
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CreateTaskScreen(modifier: Modifier = Modifier,navController: NavController? = null) {
+fun CreateTaskScreen(modifier: Modifier = Modifier, navController: NavController? = null) {
 
     val focusManager = LocalFocusManager.current
+    val scroll = rememberScrollState(0)
+    val context = LocalContext.current
+    val datePickerState = rememberDatePickerState()
+    var showDatePicker by remember { mutableStateOf(false) }
+
     var nameTask by remember { mutableStateOf("") }
     var descriptionTask by remember { mutableStateOf("") }
-    var scroll = rememberScrollState(0)
-    val context = LocalContext.current
+    val selectedDate = datePickerState.selectedDateMillis?.let {
+        convertMillisToDate(it)
+    } ?: ""
+
 
     fun isValid(): Boolean {
         return nameTask.isNotEmpty() && descriptionTask.isNotEmpty()
@@ -68,7 +94,7 @@ fun CreateTaskScreen(modifier: Modifier = Modifier,navController: NavController?
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(8.dp),
 
-    ) {
+        ) {
 
         OutlinedTextField(
             modifier = Modifier.fillMaxWidth(),
@@ -92,6 +118,62 @@ fun CreateTaskScreen(modifier: Modifier = Modifier,navController: NavController?
             }
         )
 
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = selectedDate,
+            onValueChange = {},
+            readOnly = true,
+            label = {
+                Text(text = stringResource(id = R.string.create_task_label_due_date))
+            },
+            trailingIcon = {
+                IconButton(onClick = {
+                    showDatePicker = !showDatePicker
+                }) {
+                    Icon(imageVector = Icons.Default.DateRange, contentDescription = null)
+                }
+            }
+        )
+
+        if (showDatePicker) {
+
+            DatePickerDialog(
+                colors = DatePickerDefaults.colors(
+                    containerColor = if(isSystemInDarkTheme()) MaterialTheme.colorScheme.surface else Color.White
+                ),
+                shape = RoundedCornerShape(8.dp),
+                onDismissRequest = {},
+                confirmButton = {
+                    TextButton(onClick = {
+                        showDatePicker = false
+                    }) {
+                        Text("OK")
+                    }
+                },
+                dismissButton = {
+                    TextButton(onClick = {
+                        showDatePicker = false
+                    }) {
+                        Text("Cancel")
+                    }
+                }
+            ) {
+                DatePicker(
+                    state = datePickerState,
+                    title = {
+                        Text(
+                            text = stringResource(id = R.string.create_task_label_due_date),
+                            style = TextStyle(fontSize = 20.sp),
+                            modifier = Modifier.padding(4.dp),
+                        )
+                    },
+
+                    showModeToggle = true
+                )
+            }
+
+        }
+
         Button(
             modifier = Modifier.fillMaxWidth(),
             onClick = { saveHandle() },
@@ -100,7 +182,16 @@ fun CreateTaskScreen(modifier: Modifier = Modifier,navController: NavController?
         {
             Text(text = stringResource(id = R.string.create_task_btn_text_save))
         }
+
     }
+
+
+
+}
+
+fun convertMillisToDate(millis: Long): String {
+    val formatter = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+    return formatter.format(Date(millis))
 }
 
 @Preview
